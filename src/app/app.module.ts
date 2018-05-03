2a75f635695654d3a980788a69b0c7e1fe61de18
tomasbrazas97 <tomasbrazas@gmail.com>
2018-05-04 00:44:24 +0100

    form val

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ReactiveFormsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
 /**
  * Generated class for the SignupPage page.
  *
@@ -14,8 +16,20 @@ import { TabsPage } from '../tabs/tabs';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  myform: FormGroup;
 
+  //reactive form
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.myform = new FormGroup({
+      //nested formgroup
+      userdetails: new FormGroup({
+          username: new FormControl('', [Validators.required, Validators.minLength(5)]),
+          firstname: new FormControl('', Validators.required),
+          
+      }),
+      password: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.pattern("_+\@_+\--+")), 
+    })
   }
 
   ionViewDidLoad() {
