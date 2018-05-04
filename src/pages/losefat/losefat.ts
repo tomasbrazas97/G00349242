59bc8a5b0d1d1f296bd5d05570498d39253cd049
tomasbrazas97 <tomasbrazas@gmail.com>
2018-05-04 15:47:56 +0100

    Fixes

--- src/pages/losefat/losefat.ts
+++ src/pages/losefat/losefat.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+
 /**
  * Generated class for the LosefatPage page.
  *
@@ -14,12 +16,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'losefat.html',
 })
 export class LosefatPage {
+activityLevel: string;
+weight : number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LosefatPage');
   }
 
+  calculate(){
+    this.storage.set("activityLevel", this.activityLevel);
+    this.storage.set("weight", this.weight);
+    console.log("activityLevel " + this.activityLevel);
+    console.log("weight " + this.weight);
+  }
+
+  show(){
+    this.storage.get("weight")
+    .then((data) => {
+    if (data == null) {
+      console.log("weight not in storage");
+    } else {
+      console.log("weight = " + data);
+    }
+    })
+      .catch((err) => {
+      console.log("Error = " + err);
+    })
+  }
 }
